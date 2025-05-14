9a73da412efc10a4e19e0aa7daef21799c66d82f
khalid <khalidmohammad2572@gmail.com>
2025-05-14 15:25:59 +0300

    this is for the NotificationsScreen and for the invite friends screen , and ali there's a problem with the image in the InviteFriendsScreen.dart i added the image in the assest i just want u to fix it , and for the code (for now u just made it to generated randomly)

--- lib/information_gathering/domain/entities/user_details_entity.dart
+++ lib/information_gathering/domain/entities/user_details_entity.dart
@@ -4,7 +4,9 @@ class UserDetailsEntity{
   final String lastName;
   final String phoneNumber;
   final String? address;
+  final String email;
 
-  UserDetailsEntity({required this.id, required this.firstName, required this.lastName, required this.phoneNumber, this.address});
+
+  UserDetailsEntity({required this.id, required this.firstName, required this.lastName, required this.phoneNumber,required this.email, this.address});
 
 }
