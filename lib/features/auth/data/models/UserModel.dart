cf4220293bc464150347aef005577aaeadc1a513
Abd307i <abusahab307i@gmail.com>
2025-05-19 18:34:15 +0300

    user type

--- lib/features/auth/data/models/UserModel.dart
+++ lib/features/auth/data/models/UserModel.dart
@@ -15,7 +15,7 @@ class UserModel extends UserEntity{
     required super.emailVerified,
   });
 
-  factory UserModel.fromJson(Map<String, dynamic> json){
+  factory UserModel.fromJson(Map<String, dynamic> json, String id){
     return UserModel(
         id: json['id'],
         email: json['email'],
